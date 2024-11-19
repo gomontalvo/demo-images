c7f0a345e2957012dc777148634d7db6b8aadb26
Gabriel Ulloa <gabriel.ulloa.e@gmail.com>
2024-11-19 19:45:15 -0300

    editar imagen generada previamente

--- editar.py
+++ editar.py
@@ -4,8 +4,8 @@ from openai import OpenAI
 client = OpenAI()
 
 response = client.images.edit(
-    image=open("playa.png", "rb"),
-    mask=open("mask.png", "rb"),
+    image=open("ejemplo1.png", "rb"),
+    mask=open("ejemplo1_mask.png", "rb"),
     prompt="Agrega un avion apunto de estrellarse",
     n=1,
     size="1024x1024",
